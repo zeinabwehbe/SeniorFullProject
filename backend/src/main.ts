dfa5529f4a92eace9cf592cd6676d6c35da13802
zeinabwehbe <zaynab-wehbe@hotmail.com>
2025-06-01 21:40:53 +0300

    dep

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,17 +16,6 @@ async function createApp() {
     credentials: true,
   });
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://zeinabwehbe.github.io');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-    next();
-  });
-
   app.useStaticAssets(join(__dirname, '..', '..', 'uploads'), {
     prefix: '/users/profile-picture/',
   });
