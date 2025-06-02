84966c79e8576b7a4b5d1680a84b1dd5523e9a98
zeinabwehbe <zaynab-wehbe@hotmail.com>
2025-06-02 08:35:40 +0300

    rename

--- backend/src/configuration.ts
+++ backend/src/configuration.ts
@@ -2,7 +2,26 @@ import { readYamlEnvSync } from 'yaml-env-defaults';
 
 const YML_CONFIG_FILENAME = 'application.yml';
 
-const config = readYamlEnvSync(YML_CONFIG_FILENAME);
+//const config = readYamlEnvSync(YML_CONFIG_FILENAME);
+
+require('dotenv').config();
+
+const config = {
+  application: {
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 3000,
+    name: 'test',
+    'application-root': '/api/v1'
+  },
+  bcrypt: {
+    saltRounds: 10
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET
+  }
+};
+
+module.exports = config;
 
 /**
  * It is responsible for reading the configuration from the application.yml
