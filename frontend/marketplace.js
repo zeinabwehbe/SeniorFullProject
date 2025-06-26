a9c4ccb41570b153ca799d07b60199cb57000176
zeinabwehbe <zaynab-wehbe@hotmail.com>
2025-06-27 02:03:30 +0300

    test

--- frontend/marketplace.js
+++ frontend/marketplace.js
@@ -1,5 +1,5 @@
-const API_URL = 'http://localhost:3000';
-//const API_URL ="https://seniorfullproject-production.up.railway.app";
+//const API_URL = 'http://localhost:3000';
+const API_URL ="https://seniorfullproject-production.up.railway.app";
 
 const USER_SKILLS_URL = `${API_URL}/user-skills`;
 const CATEGORIES_URL = `${API_URL}/categories`;
